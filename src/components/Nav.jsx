1ae9bca5c69f3fccb6df5714ce205079cd397768
iamgunbun <ammonsgunner@gmail.com>
2025-08-25 21:27:13 -0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,12 +11,7 @@ const Nav = ({ theme, toggleTheme, setPage }) => {
   return (
     <nav className="nav">
       <div className="nav-container">
-        <div className="nav-left">
-          {/* Mobile Menu Button (Hamburger) */}
-          <button className="mobile-menu-button" onClick={toggleMobileMenu}>
-            &#9776; {/* Unicode for hamburger icon */}
-          </button>
-        </div>
+        {/* The nav-left div has been removed to center the content */}
         <div className="nav-logo">
           <img 
             src="/static/tbelogo.png" 
@@ -24,29 +19,28 @@ const Nav = ({ theme, toggleTheme, setPage }) => {
             className="nav-logo-image"
           />
         </div>
-        <div className={`nav-right ${isMobileMenuOpen ? 'open' : ''}`}>
-          <ul className="nav-links">
-            {siteData.navItems.map((item, index) => (
-              <li key={index}>
-                <a 
-                  href={item.href || '#'} 
-                  onClick={() => {
-                    if (item.page) {
-                      setPage(item.page);
-                    }
-                    setIsMobileMenuOpen(false);
-                  }}
-                  className="nav-link-item"
-                >
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <button onClick={toggleTheme} className="theme-toggle-button">
-            {theme === 'dark' ? '☀️' : '🌙'}
-          </button>
-        </div>
+        {/* The nav-right div has been removed */}
+        <ul className="nav-links">
+          {siteData.navItems.map((item, index) => (
+            <li key={index}>
+              <a 
+                href={item.href || '#'} 
+                onClick={() => {
+                  if (item.page) {
+                    setPage(item.page);
+                  }
+                  setIsMobileMenuOpen(false);
+                }}
+                className="nav-link-item"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <button onClick={toggleTheme} className="theme-toggle-button">
+          {theme === 'dark' ? '☀️' : '🌙'}
+        </button>
       </div>
     </nav>
   );
